6aa95f6eccc1c9e8e725416b51869e9394f8ae2b
Yadu Nandan <57453391+Y-D-U@users.noreply.github.com>
2022-08-12 17:55:20 +0530

    Create NextPermutation.cpp

---
+++ Arrays/NextPermutation.cpp
@@ -0,0 +1,46 @@
+/*
+Given an array Arr[] of integers, rearrange the numbers of the given array into the lexicographically next greater permutation of numbers.
+
+If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).*/
+/*
+Input format: Arr[] = {1,3,2}
+
+Output: Arr[] = {2,1,3}
+
+Explanation: All permutations of {1,2,3} are {{1,2,3} , {1,3,2}, {2,13} , {2,3,1} , {3,1,2} , {3,2,1}}. So, the next permutation just after {1,3,2} is {2,1,3}.
+
+
+Example 2:
+
+Input format: Arr[] = {3,2,1}
+
+Output: Arr[] = {1,2,3}
+
+Explanation: As we see all permutations of {1,2,3}, we find {3,2,1} at the last position. So, we have to return the topmost permutation.*/
+
+#include <bits/stdc++.h> 
+vector<int> nextPermutation(vector<int> &permutation, int n)
+{
+    //  Write your code here.
+    int ind=-1;
+    for(int i=n-2;i>=0;--i){       
+        if(permutation[i]<permutation[i+1]){
+            ind=i;
+            break;
+        }}
+    if(ind==-1){
+        reverse(permutation.begin(),permutation.end());
+        return permutation;
+    }
+    for(int i=n-1;i>=0;--i){
+        if(permutation[i]>permutation[ind]){
+            swap(permutation[ind],permutation[i]);
+            break;
+        }
+    }
+    reverse(permutation.begin()+ind+1,permutation.end());
+        
+        return permutation;
+        
+    
+}
